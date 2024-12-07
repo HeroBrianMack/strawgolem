faba4e6ec86e357745d3ec0bf1c69bc3b791587a
Hero <herostillplays@gmail.com>
2024-12-06 22:05:08 -0500

    Deprecating old canSee() method, replacing with isNearby() for future golem AI usage.

--- common/src/main/java/com/t2pellet/strawgolem/util/VisibilityUtil.java
+++ common/src/main/java/com/t2pellet/strawgolem/util/VisibilityUtil.java
@@ -1,5 +1,6 @@
 package com.t2pellet.strawgolem.util;
 
+import com.t2pellet.strawgolem.StrawgolemConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
@@ -13,14 +14,15 @@ public class VisibilityUtil {
 
     private VisibilityUtil() {}
 
+    @Deprecated
     public static boolean canSee(LivingEntity e, BlockPos query) {
-        Level level = e.level();
-        Vec3 entityPos = e.getEyePosition();
-        Vec3 queryPos = Vec3.atCenterOf(query);
-        ClipContext ctx = new ClipContext(entityPos, queryPos, ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, e);
-        BlockHitResult result = level.clip(ctx);
-        ClipContext ctx2 = new ClipContext(entityPos, queryPos, ClipContext.Block.VISUAL, ClipContext.Fluid.NONE, e);
-        BlockHitResult result2 = level.clip(ctx2);
+//        Level level = e.level();
+//        Vec3 entityPos = e.getEyePosition();
+//        Vec3 queryPos = Vec3.atCenterOf(query);
+//        ClipContext ctx = new ClipContext(entityPos, queryPos, ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, e);
+//        BlockHitResult result = level.clip(ctx);
+//        ClipContext ctx2 = new ClipContext(entityPos, queryPos, ClipContext.Block.VISUAL, ClipContext.Fluid.NONE, e);
+//        BlockHitResult result2 = level.clip(ctx2);
 
 //        return result.getBlockPos().equals(query) || result.getBlockPos().equals(result2);
         return isNearby(e, query);
@@ -28,16 +30,12 @@ public class VisibilityUtil {
 
 
     public static boolean isNearby(LivingEntity e, BlockPos block) {
-        int ey = e.getBlockY();
-        int by = block.getY();
-        int ez = e.getBlockZ();
-        int bz = block.getZ();
         int distanceX = Math.abs(e.getBlockX() - block.getX());
         int distanceY = Math.abs(e.getBlockY() - block.getY());
         int distanceZ = Math.abs(e.getBlockZ() - block.getZ());
-        // If block is within 15 blocks front/back/left/right
-        // and within 2 above or below, this should see it.
-        if (distanceX > 15 || distanceY > 3 || distanceZ > 15 ) {
+        // If block is within harvest range front/back/left/right
+        // and within 2 above or below, the golem should see it.
+        if (distanceX > StrawgolemConfig.Harvesting.harvestRange.get() || distanceY > 3 || distanceZ > StrawgolemConfig.Harvesting.harvestRange.get()) {
             return false;
         }
         return true;
